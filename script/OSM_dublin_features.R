309f58744108c4337243619d0f8683a8c9d42e02
damien-dupre <damien.dupre.ac@gmail.com>
2019-02-20 15:57:31 +0000

    fearture importance

---
+++ script/OSM_dublin_features.R
@@ -0,0 +1,231 @@
+amenity <- c(
+  "amenity_bar",
+  "amenity_college",                             
+  "amenity_school",
+  "amenity_university",                          
+  "amenity_bicycle_parking",
+  "amenity_fuel",                               
+  "amenity_parking",
+  "amenity_community_centre",                    
+  "amenity_bench",
+  "amenity_embassy",                            
+  "amenity_police",
+  "amenity_prison",                             
+  "amenity_recycling"
+)
+barrier <- c(
+  "barrier_city_wall",                           
+  "barrier_ditch",                              
+  "barrier_fence",                               
+  "barrier_guard_rail",                         
+  "barrier_hedge",                              
+  "barrier_kerb",                               
+  "barrier_retaining_wall",                      
+  "barrier_wall",                               
+  "barrier_block",                               
+  "barrier_bollard",                            
+  "barrier_chain",                               
+  "barrier_full-height_turnstile",              
+  "barrier_gate",                                
+  "barrier_jersey_barrier",                     
+  "barrier_yes"
+)
+boundary <- c(
+  "boundary_administrative",                    
+  "boundary_historic",                           
+  "boundary_political",                         
+  "boundary_postal_code",                        
+  "boundary_protected_area"
+)
+building <- c(
+  "building_apartments",                         
+  "building_house",                             
+  "building_residential",                        
+  "building_commercial",                        
+  "building_industrial",                         
+  "building_retail",                           
+  "building_hospital",                           
+  "building_university",                        
+  "building_yes"
+)
+highway <- c(
+  "highway_motorway",                           
+  "highway_trunk",                              
+  "highway_secondary",                          
+  "highway_tertiary",                            
+  "highway_unclassified",                       
+  "highway_residential",                         
+  "highway_service",                            
+  "highway_motorway_link",                       
+  "highway_trunk_link",                         
+  "highway_secondary_link",                      
+  "highway_tertiary_link",                      
+  "highway_pedestrian",                          
+  "highway_track",                              
+  "highway_road",                                
+  "highway_footway",                            
+  "highway_steps",                               
+  "highway_path",                               
+  "highway_cycleway"
+)
+cycleway <- c(
+  "cycleway_lane",                              
+  "cycleway_opposite",                          
+  "cycleway_opposite_lane",                     
+  "cycleway_track",                              
+  "cycleway_share_busway",                      
+  "cycleway_shared_lane"
+)
+busway <- c(
+  "busway_lane"
+)
+highway <- c(
+  "highway_proposed",                            
+  "highway_construction" 
+)
+junction <- c(
+  "junction_roundabout"
+)
+historic <- c(
+  "historic_yes"
+)
+landuse <- c(
+  "landuse_commercial",                          
+  "landuse_construction",                       
+  "landuse_industrial" ,                         
+  "landuse_residential",                        
+  "landuse_retail",                              
+  "landuse_farmland",                           
+  "landuse_grass",                               
+  "landuse_military",                           
+  "landuse_railway",                             
+  "landuse_recreation_ground",                  
+  "landuse_religious"
+)
+leisure <- c(
+  "leisure_nature_reserve",                     
+  "leisure_park",                                
+  "leisure_slipway",                            
+  "leisure_sports_centre",                      
+  "leisure_stadium",                            
+  "leisure_track"
+)
+man_made <- c(
+  "man_made_breakwater",                        
+  "man_made_crane",                              
+  "man_made_embankment",                        
+  "man_made_groyne",                            
+  "man_made_pier",                              
+  "man_made_pipeline"
+)
+natural <- c(
+  "natural_wood",                               
+  "natural_tree_row",                            
+  "natural_scrub",                             
+  "natural_grassland",                           
+  "natural_water",                              
+  "natural_beach",                               
+  "natural_coastline",                          
+  "natural_ridge",                               
+  "natural_cliff"
+)
+place <- c(
+  "place_district",                              
+  "place_county",                               
+  "place_city",                                  
+  "place_suburb",                               
+  "place_island",                                
+  "place_locality"
+)
+power <- c(
+  "power_cable",
+  "power_line",
+  "power_minor_line",                           
+  "power_portal" 
+)
+line <- c(
+  "line_busbar"
+)
+public_transport <- c(
+  "public_transport_platform",                 
+  "public_transport_stop_area"                  
+)
+railway <- c(
+  "railway_abandoned",                          
+  "railway_disused",                             
+  "railway_rail",                               
+  "railway_tram",
+  "railway_platform"
+)
+bridge <- c(
+  "bridge_yes"
+)
+cutting <- c(
+  "cutting_yes"                                 
+)
+electrified_contact <- c(
+  "electrified_contact_line"                   
+)
+embankment <- c(
+  "embankment_yes"                              
+)
+service <- c(
+  "service_crossover",                          
+  "service_siding",                              
+  "service_spur",
+  "service_yard"                                
+)
+tunnel <- c(
+  "tunnel_yes"
+)
+usage <- c(
+  "usage_main"                                  
+)
+route <- c(                       
+  "route_bicycle",                               
+  "route_bus",                                  
+  "route_ferry",                                 
+  "route_hiking",                               
+  "route_power",                                 
+  "route_road",                                 
+  "route_train",                                 
+  "route_tram"
+)                                 
+shop <- c(  
+  "shop_paint",                                  
+  "shop_kitchen",                               
+  "sport_badminton",                             
+  "sport_equestrian",                           
+  "sport_gaelic_games",                          
+  "sport_rugby_union",                          
+  "sport_running"
+)
+tourism <- c(
+  "tourism_artwork",                            
+  "tourism_zoo"                                 
+)
+waterway <- c(
+  "waterway_river",                             
+  "waterway_riverbank",                          
+  "waterway_stream",                            
+  "waterway_canal",                              
+  "waterway_drain",                            
+  "waterway_ditch",                              
+  "waterway_weir",                              
+  "waterway_lock_gate"
+)
+source <- c(
+  "source_survey"                              
+)
+area <- c(
+  "area_yes"
+)
+covered <- c(
+  "covered_yes" 
+)
+disused <- c(
+  "disused_yes"
+)
+tidal <- c(
+  "tidal_yes" 
+)
