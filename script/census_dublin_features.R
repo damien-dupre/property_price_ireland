1b7a7e39ff227f34e7a65a3388b90a6e66bbd87a
damien-dupre <damien.dupre.ac@gmail.com>
2019-02-22 18:41:47 +0000

    paper with prediction accuracy/error

---
+++ script/census_dublin_features.R
@@ -0,0 +1,66 @@
+carers <- c(              
+  "% Provides No Care",                          
+  "% 1-19 hours unpaid PW",                     
+  "% 20-49 hours unpaid PW",                     
+  "% 50+ hours unpaid PW",                      
+  "% Total Care Providers"                      
+)
+disabilty_age_group <- c(
+  "% Persons with a disability aged 0 - 14",      
+  "% Persons with a disability aged 15 - 44",    
+  "% Persons with a disability aged 25 - 44",   
+  "% Persons with a disability aged 45 - 64",    
+  "% Persons with a disability aged 65 Plus",   
+  "% Total persons with a disability" 
+)
+general_health <- c(
+  "% Very Good",                                
+  "% Good",                                     
+  "% Fair",                                     
+  "% Bad",                                       
+  "% Very Bad"
+)
+housing_occupancy <- c(
+  "% Occupied/HS With ususal Residents",         
+  "% Unoccupied/HS Without Ususal Residents" 
+)
+housing_rooms <- c(
+  "% 1 Room (Households)",                      
+  "% 2 Rooms (Households)",                      
+  "% 3 Rooms (Households)",                     
+  "% 4 Rooms (Households)",                      
+  "% 5 Rooms (Households)",                     
+  "% 6 Rooms (Households)",                      
+  "% 7 Rooms (Households)",                     
+  "% 8 or more Rooms (Households)"
+)
+housing_tenure <- c(
+  "% Owner Occupier with Mortgage (Households)",
+  "% Owner Occupier No Mortgage (Households)",   
+  "% Private Rented",                           
+  "% Social Rented",                             
+  "% Rented Free of Rent (Households)" 
+)                               
+housing_type <- c(
+  "% House/Bungalow",                           
+  "% Flat/Apartment/BedSit",                     
+  "% Caravan/Mobile home/Temperory" 
+)               
+population <- c(
+  "% Male",                                     
+  "% Female",                                  
+  "% Age 0 - 14",                                   
+  "% Age 15 - 24",                                 
+  "% Age 25 - 44",                                  
+  "% Age 45 - 64",                                  
+  "% Age 65 Plus",                                    
+  "% Age 15- 64",                                  
+  "% Age 80 Plus"
+)
+religion <- c(
+  "% Roman Catholic",                                 
+  "% Other Catholic",                                   
+  "% Other Religion",                                  
+  "% No Answer",                                       
+  "% No Religion"
+)
